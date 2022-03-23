18ee89dadf9fd8b898303668c76040e08b8b9b65
Heitor Casella Torrezan <heitorcasella@gmail.com>
2022-03-22 21:48:05 -0300

    Tratando

--- controller/contatoControle.php
+++ controller/contatoControle.php
@@ -1,8 +1,18 @@
 <?php
 
+// Determina se alguma das informações estão vazias, retornando um erro
+if ($_POST['nome'] === "" || $_POST['email'] === "" || $_POST['mensagem'] === "") {
+    if ($_POST['nome'] === "") {
+        $retorno['nomeVazio'] = true;
+    }
+    if ($_POST['email'] === "") {
+        $retorno['emailVazio'] = true;
+    }
+    if ($_POST['mensagem'] === "") {
+        $retorno['mensagemVazio'] = true;
+    }
+}
 
-$retorno['post'] = $_POST;
-$retorno['status'] = "entrouA";
 echo json_encode($retorno);
 
 ?>
